114e4301b6a7bdf17fa49e5c5868f0153a017dde
Hack Kampbjørn <hkampbjorn@go7.io>
2026-02-16 11:48:52 +0100

    chore: update version to 1.0.3.dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'pytouchline_extended',
   packages = ['pytouchline_extended'],
-  version = "$GITHUB_REF_NAME",
+  version = "1.0.3.dev1",
   description = 'A Roth Touchline interface library',
   long_description="A simple helper library for controlling a Roth Touchline heat pump controller",
   author = 'Peter Brondum',
